418b369a6e053ca76ee52bbdb90e613dc7e94765
AlejandroFeriaGonzalez <aferiag@unal.edu.co>
2024-12-21 12:29:13 -0500

    db

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,14 +85,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # }
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
-    }
+    'default': dj_database_url.parse('postgres://postgres:X5Gm4VhQtK40DI9zriSPy5hIiQjrFv6DXVJexqR1ZYRqaHw7UogJR9ZcQdL7hBh6@mo8wwcoocwsk8kwk00cs0os8:5432/postgres')
 }
 
 
